694616ff9168f254a3f8b4c98cc0ea0bb089e60b
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-10 23:29:12 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
-
 const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// Make .env file data accessible
+dotenv.config();
+
+// Connecting to mongoDB
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`Connected to MongoDB!`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Middlewares
+app.use(express.json()); // This middleware parse the data that I post(send) to the server and {{{Make it available in the req.body}}}
+app.use(helmet());
+app.use(morgan("common"));
 
 app.listen("8800", () => {
   console.log("Backend server is running!");
