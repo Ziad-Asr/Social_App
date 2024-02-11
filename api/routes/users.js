c5b71b99f9d7113a052cc132139ed7c79907aa0c
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-11 07:08:32 +0200

    update

--- api/routes/users.js
+++ api/routes/users.js
@@ -54,6 +54,29 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+// Follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+
+        res.status(200).json("User has been followed");
+      } else {
+        res.status(403).json("You allredy follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can not follow yourself");
+  }
+});
+
 module.exports = router;
 
 // In Jonous course he made two different things than here
