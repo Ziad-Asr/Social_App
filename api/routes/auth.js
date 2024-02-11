3c40d7964aa776d8bfb33d67b44501a084f731f5
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-11 02:27:36 +0200

    update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,52 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res, next) => {
-  res.send("Hey this is auth router!");
+const User = require("../models/User");
+
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    !user &&
+      res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
 });
 
 module.exports = router;
